d24bc522d59d370b0bcabd74d1ad6ad38052af2b
YukiNabeshima <yukinabe3561@icloud.com>
2016-08-25 16:25:20 +0900

    fix edit bugs

--- app/src/main/java/com/valkyrie/nabeshimamac/lightsout/LightsOutView.java
+++ app/src/main/java/com/valkyrie/nabeshimamac/lightsout/LightsOutView.java
@@ -173,6 +173,9 @@ public class LightsOutView extends LinearLayout implements View.OnClickListener 
     }
 
     public void setFlagsFromString(String flagPositions) {
+        if (flagPositions.length() != boardSize * boardSize) {
+            return;
+        }
         for (int i = 0; i < boardSize; i++) {
             for (int j = 0; j < boardSize; j++) {
                 flag[i][j] = flagPositions.charAt(i * boardSize + j) == '1';
--- app/src/main/java/com/valkyrie/nabeshimamac/lightsout/MakeActivity.java
+++ app/src/main/java/com/valkyrie/nabeshimamac/lightsout/MakeActivity.java
@@ -1,7 +1,6 @@
 package com.valkyrie.nabeshimamac.lightsout;
 
 import android.content.DialogInterface;
-import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -19,51 +18,22 @@ import android.widget.TextView;
 
 import com.activeandroid.query.Select;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 public class MakeActivity extends AppCompatActivity {
     private LightsOutView lightsOutEachView;
-    private List<Point> prePoints;
     EditText editText;
     TextView detailText;
     Spinner spinner;
     long questionId;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make);
-
-        setTitle("新規問題作成・編集");
-
-        detailText = (TextView)findViewById(R.id.detaleTextView);
-
-        spinner = (Spinner)findViewById(R.id.spinner);
-        final ArrayAdapter spinnerAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.size_spinner));
-        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(spinnerAdapter);
-
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                if(position == 0) {
-                    lightsOutEachView.setBoardSize(5);
-                } else {
-                    lightsOutEachView.setBoardSize(6);
-                }
-                updateDetailsText();
-            }
-            @Override
-            public void onNothingSelected(AdapterView<?>adapterView){
-
-            }
-        });
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        setTitle("新規問題作成・編集");
         toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
@@ -71,6 +41,13 @@ public class MakeActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        detailText = (TextView)findViewById(R.id.detaleTextView);
+
+        spinner = (Spinner)findViewById(R.id.spinner);
+        final ArrayAdapter spinnerAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.size_spinner));
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(spinnerAdapter);
 
         lightsOutEachView = (LightsOutView) findViewById(R.id.lightsOutView2);
         lightsOutEachView.setMode(LightsOutView.MODE_MAKE);
@@ -83,16 +60,39 @@ public class MakeActivity extends AppCompatActivity {
             public void onClearListener() {
             }
         });
-        prePoints = new ArrayList<>();
         editText = (EditText) findViewById(R.id.titleEditText);
 
         questionId = getIntent().getLongExtra("question_id", -1);
         if (questionId == -1) {
+            spinner.setEnabled(true);
             // 新規作成
+            spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    if(position == 0) {
+                        lightsOutEachView.setBoardSize(5);
+                    } else {
+                        lightsOutEachView.setBoardSize(6);
+                    }
+                    updateDetailsText();
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+
+                }
+            });
         } else {
             // 編集
             Question question = new Select().from(Question.class).where("id = ?", questionId).executeSingle();
             editText.setText("" + question.title);
+            spinner.setEnabled(false);
+            if (question.size == 5) {
+                spinner.setSelection(0, false);
+            } else {
+                spinner.setSelection(1, false);
+            }
+            lightsOutEachView.setBoardSize(question.size);
             lightsOutEachView.setFlagsFromString(question.board);
             lightsOutEachView.updateFlags();
         }
@@ -157,7 +157,6 @@ public class MakeActivity extends AppCompatActivity {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
                             //  自動生成されたメソッド・スタブ
-
                         }
                     })
                     .show();
