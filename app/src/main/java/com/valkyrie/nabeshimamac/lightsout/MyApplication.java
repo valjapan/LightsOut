b4ac8cd438525d2e309f6641003250f5fbe90cf9
YukiNabeshima <yukinabe3561@icloud.com>
2016-08-25 14:17:32 +0900

    add firebase gradle settings

--- app/src/main/java/com/valkyrie/nabeshimamac/lightsout/MyApplication.java
+++ app/src/main/java/com/valkyrie/nabeshimamac/lightsout/MyApplication.java
@@ -5,24 +5,29 @@ import android.support.annotation.NonNull;
 import com.activeandroid.app.Application;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.games.Games;
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 /**
  * Created by NabeshimaMAC on 16/08/20.
  */
 public class MyApplication extends Application {
     @NonNull
-    private GoogleApiClient mGoogleApiClient;
+    private GoogleApiClient googleApiClient;
+    private FirebaseAnalytics analytics;
 
     @Override
     public void onCreate() {
         super.onCreate();
 
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
+        googleApiClient = new GoogleApiClient.Builder(this)
                 .addApi(Games.API)
                 .addScope(Games.SCOPE_GAMES)
                 .build();
+
+        analytics = FirebaseAnalytics.getInstance(this);
     }
+
     public GoogleApiClient getGoogleApiClient() {
-        return mGoogleApiClient;
+        return googleApiClient;
     }
 }
